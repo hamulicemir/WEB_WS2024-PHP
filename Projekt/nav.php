7b65966ab7e4be67b00984dde13bd5dbd917f5de
Adi Redzic <wi23b112@technikum-wien.at>
2024-10-21 18:54:11 +0200

    Edit nav.php

---
+++ Projekt/nav.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./login.stylesheet.css" rel="stylesheet"/> 
+    <title>User-Login</title>
+</head>
+<body id="login-body">
+    <div class="background"></div>
+        <div class="login-box">
+            <h2>Login</h2>
+                <form action="./test.html" method="post">
+                <div class="textbox">
+                    <label>Benutzername<input type="text" placeholder="Benutzername" name="username" required></label>
+                </div>
+                <div class="textbox">
+                    <label>Passwort<input type="password" placeholder="Passwort" name="password" required></label>
+                </div>
+                    <input type="submit" class="btn" value="Einloggen">
+                </form>
+        </div>
+</body>
+</html>
