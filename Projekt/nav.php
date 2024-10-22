d20fb34f6b9e53a609fc533a91c076f26794785a
Emir Hamulic <hamulicemir@icloud.com>
2024-10-22 18:42:25 +0200

    finished registration

--- Projekt/nav.php
+++ Projekt/nav.php
@@ -10,8 +10,8 @@
     </style>
 </head>
 <body>
-<div class="container" style="">
-        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom pb-5">
+<div class="container">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-2 border-bottom">
           <div class="col-md-3 mb-2 mb-md-0">
             <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
               <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
@@ -24,7 +24,7 @@
             <li><a href="./FAQ.php" class="nav-link px-2">FAQs</a></li>
             <li><a href="./impressum.php" class="nav-link px-2">Impressum</a></li>
           </ul>
-    
+
           <div class="col-md-3 text-end">
             <a href="./login.php"><button type="button" class="btn btn-outline-primary me-2">Login</button></a>
             <a href="./registration.php"><button type="button" class="btn btn-primary">Sign-up</button></a>
--- Projekt/registration.php
+++ Projekt/registration.php
@@ -16,59 +16,123 @@
             z-index: -1;
             filter: blur(5px);
         }
-        .container-fluid {
-            padding-top: 60px; /* Abstand für die Navbar */
-        }
-        .navbar {
-            z-index: 1; /* Sicherstellen, dass die Navbar über dem Hintergrund ist */
-            position: fixed;
-            width: 100%;
-        }
     </style>
 </head>
-<body>
-<?php include 'nav.php'; ?>
-    <div class="background"></div>
-    <div class="container-fluid vh-100 d-flex justify-content-center align-items-center pt-5">
-        <div class="row w-100">
-            <div class="col-12 col-md-4 form-container mx-auto">
-                <div class="card p-4">
-                    <h2 class="fw-bold mb-3 mx-auto">Registration</h2>
-                    <form action="./registration.html" method="post">
-                        <div class="form-floating mb-3">
-                            <select class="form-select" id="anrede" name="formAnrede" aria-label="Floating label select example" required>
-                                <option value="" disabled selected>Wählen Sie Ihre Anrede</option>
-                                <option value="formFrau">Frau</option>
-                                <option value="formMann">Mann</option>
-                            </select>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input class="form-control" type="text" id="vorname" name="formVorname" placeholder="Vorname" required>
-                            <label for="vorname">Vorname</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input class="form-control" type="text" id="nachname" name="formNachname" placeholder="Nachname" required>
-                            <label for="nachname">Nachname</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input class="form-control" type="text" id="nachname" name="formNachname" placeholder="Nachname" required>
-                            <label for="nachname">Nachname</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input class="form-control" type="text" id="nachname" name="formNachname" placeholder="Nachname" required>
-                            <label for="nachname">Nachname</label>
-                        </div>
-                        
-                        <p class="small mt-2"><a class="text-black" href="#!">Already signed up?</a></p>
-                        <button class="btn btn-primary btn-lg btn-block w-100" type="submit">Sign-Up</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 
+<body>
+    <?php include 'nav.php'; ?>
+    <div class="background"></div>
+    <section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row justify-content-center align-items-center h-100">
+      <div class="col-12 col-lg-9 col-xl-7">
+        <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
+          <div class="card-body p-4 p-md-5">
+            <h2 class="fw-bold mb-3 mx-auto text-center">Sign-Up</h2>
+            <form>
+              <div class="row">
+                <div class="col-md-6 mb-2">
+
+                  <div class="form-floating mb-2">
+                    <input type="text" id="firstName" class="form-control form-control-lg" placeholder="first name" />
+                    <label for="firstName">First Name</label>
+                  </div>
+
+                </div>
+                <div class="col-md-6 mb-2">
+
+                <div class="form-floating mb-2">
+                    <input type="text" id="lastName" class="form-control form-control-lg" placeholder="last name" />
+                    <label for="lastName">Last Name</label>
+                </div>
+
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6 mb-2 d-flex align-items-center">
+
+                <div class="form-floating mb-2 datepicker w-100">
+                    <input type="date" id="birthdayDate" class="form-control form-control-lg" placeholder="first name" />
+                    <label for="birthdayDate">Birthday</label>
+                </div>
+
+                </div>
+                <div class="col-md-6 mb-2">
+
+                  <h6 class="mb-2 pb-1">Gender: </h6>
+
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="femaleForm" id="femaleGender"
+                      value="female" checked />
+                    <label class="form-check-label" for="femaleGender">Female</label>
+                  </div>
+
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="maleForm" id="maleGender"
+                      value="male"/>
+                    <label class="form-check-label" for="maleGender">Male</label>
+                  </div>
+
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="otherForm" id="otherGender"
+                      value="other"/>
+                    <label class="form-check-label" for="otherGender">Other</label>
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6 mb-2 pb-2">
+
+                <div class="form-floating mb-1">
+                    <input type="email" id="emailAddress" class="form-control form-control-lg" placeholder="name@example.com" />
+                    <label class="form-label" for="emailAddress">E-Mail</label>
+                </div>
+
+                </div>
+                <div class="col-md-6 mb-2 pb-2">
+
+                <div class="form-floating mb-2">
+                    <input type="text" id="username" class="form-control form-control-lg" placeholder="Username" />
+                    <label for="username">Username</label>
+                </div>
+
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6 mb-2 pb-2">
+                    <div class="form-floating mb-2">
+                        <input type="password" id="passwordfirst" class="form-control form-control-lg" placeholder="password" />
+                        <label for="passwordfirst">Password</label>
+                    </div>
+                </div>
+
+                <div class="col-md-6 mb-1 pb-1">
+                    <div class="form-floating mb-2">
+                        <input type="password" id="passwordsecond" class="form-control form-control-lg" placeholder="password" />
+                        <label for="passwordsecond">Repeat Password</label>
+                    </div>
+                </div>
+                <p class="small"><a class="text-black" href="#!">Bereits registriert?</a></p>
+              </div>
+
+              <div class="">
+                <button class="btn btn-primary btn-lg btn-block w-100" type="submit">Sign-Up</button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    
+</body>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
 </html>
