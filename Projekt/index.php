c03d84c03d1851cf98dfa53525c296b67689ff10
Emir Hamulic <hamulicemir@icloud.com>
2024-12-26 18:47:13 +0100

    added sign up functionality and modal

--- Projekt/index.php
+++ Projekt/index.php
@@ -15,6 +15,5 @@
     </div>
   </div>
   <?php include './inc/footer.php'; ?>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+3oB7j1o5y5r1b7+AMvyTG2x1p5r" crossorigin="anonymous"></script>
 </body>
 </html> 
