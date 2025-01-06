bbba40bb91bb4b060b372024b327d3de1657f8c3
Emir Hamulic <hamulicemir@icloud.com>
2025-01-06 15:31:06 +0100

    added pictures description

--- Projekt/pictures.php
+++ Projekt/pictures.php
@@ -35,8 +35,7 @@ if (session_status() === PHP_SESSION_NONE) {
                         <img src="./Pictures/alkoholicari.jpg" class="img-fluid" alt="Responsive image">
                         <div class="card-body">
                             <p class="card-text">
-                                This is text.
-                            </p>
+                                Indulge in luxury at its finest with our exclusive romantic getaway package. Revel in the elegance of our plush rooms, featuring sophisticated décor and a cozy atmosphere. Celebrate life's special moments with a bottle of Moët & Chandon champagne, delectable gourmet desserts, and the finest touches of comfort, all designed to make your stay unforgettable. Let us help you create memories that will last a lifetime.                            </p>
                         </div>
                     </div>
                 </div>
@@ -46,7 +45,7 @@ if (session_status() === PHP_SESSION_NONE) {
                     <img src="./Pictures/soba_zena.jpg" class="img-thumbnail" alt="Responsive image">
                     <div class="card-body">
                             <p class="card-text">
-                                This is text.
+                            Escape to a tranquil oasis where nature meets comfort. Our eco-luxury suites offer a harmonious blend of rustic charm and modern elegance, complete with canopy beds, warm wooden interiors, and breathtaking views of lush greenery. Start your day with the serenity of the outdoors, as you unwind on your private balcony and embrace the peaceful rhythm of nature. Your perfect retreat awaits.
                             </p>
                         </div>
                     </div>
@@ -57,8 +56,7 @@ if (session_status() === PHP_SESSION_NONE) {
                         <img src="./Pictures/pexels-pixabay-261101.jpg" class="img-thumbnail" alt="Responsive image">
                         <div class="card-body">
                             <p class="card-text">
-                                This is text.
-                            </p>
+                                Welcome to our tropical oasis, where every day feels like a vacation. Our resort-style pool is surrounded by swaying palm trees and serene landscaping, creating the perfect environment for relaxation and fun. Whether you're lounging poolside, taking a refreshing dip, or enjoying a quiet moment under our charming cabana, you'll find everything you need to unwind and soak up the sun. Your paradise escape awaits!                            </p>
                         </div>
                     </div>
                 </div>
